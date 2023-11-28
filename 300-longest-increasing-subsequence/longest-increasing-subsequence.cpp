7db7ad9f6f994748ca3d10a14088ca6f67bd72f5
MayurJagtap096 <96325124+MayurJagtap096@users.noreply.github.com>
2023-11-28 23:30:49 +0530

    Time: 10 ms (80.83%) | Memory: 10.8 MB (62.09%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) 
+    {
+        int n = nums.size();
+    vector<int> ans;
+ 
+    // Initialize the answer vector with the
+    // first element of nums
+    ans.push_back(nums[0]);
+ 
+    for (int i = 1; i < n; i++) {
+        if (nums[i] > ans.back()) {
+            ans.push_back(nums[i]);
+        }
+        else {
+            int low = lower_bound(ans.begin(), ans.end(),nums[i]) - ans.begin();
+            ans[low] = nums[i];
+        }
+    }
+    return ans.size();
+    }
+};
