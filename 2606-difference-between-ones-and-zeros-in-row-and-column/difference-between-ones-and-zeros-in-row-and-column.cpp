86e27aeb4b77a5a16356d4ee77fb54e8b9aabc12
MayurJagtap096 <96325124+MayurJagtap096@users.noreply.github.com>
2023-12-14 10:15:46 +0530

    Time: 156 ms (99.60%) | Memory: 116.5 MB (93.99%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<int> row1(m,0),row0(m,0);
-        vector<int> col1(n,0),col0(n,0);
-        vector<vector<int>> ans;
+        int m = grid.size(),n = grid[0].size();
+        vector<int> row1(m,0);
+        vector<int> col1(n,0);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==0){
-                    row0[i]++;
-                    col0[j]++;
-                }
-                if(grid[i][j]==1){
-                    row1[i]++;
-                    col1[j]++;
-                }
+                row1[i]+=grid[i][j];
+                col1[j]+=grid[i][j];
             }
         }
         for(int i=0;i<m;i++){
-            vector<int> temp;
             for(int j=0;j<n;j++){
-                temp.push_back(row1[i]+col1[j]-row0[i]-col0[j]);
+                grid[i][j] = 2*(row1[i]+col1[j]) - m - n;
             }
-            ans.push_back(temp);
         }
-        return ans;
+        return grid;
     }
 };
