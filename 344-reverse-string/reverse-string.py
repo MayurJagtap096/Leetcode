da2afff96c19b0862f62de8ae63a1e6cdc9109c4
MayurJagtap096 <96325124+MayurJagtap096@users.noreply.github.com>
2024-06-02 10:23:56 +0530

    Time: 173 ms (33.73%) | Memory: 20.8 MB (69.97%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        for i in range(0,int(len(s)/2)):
+            temp = s[i] 
+            s[i] = s[len(s)-i-1]
+            s[len(s)-i-1] = temp
+            
