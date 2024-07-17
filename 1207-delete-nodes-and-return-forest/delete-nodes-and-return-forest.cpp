9c219b82eb01fdebf00480b5305ad8f9b0f6004f
MayurJagtap096 <96325124+MayurJagtap096@users.noreply.github.com>
2024-07-17 20:56:13 +0530

    Time: 21 ms (23.87%) | Memory: 27.1 MB (23.69%) - LeetSync

---
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* solve(TreeNode* root, unordered_set<int> &st, vector<TreeNode*> &ans) {
+        if(root==NULL) return NULL;
+
+        root->left = solve(root->left, st, ans);
+        root->right = solve(root->right, st, ans);
+
+        if(st.find(root->val) != st.end()) {
+            if(root->left != NULL) ans.push_back(root->left);
+            if(root->right != NULL) ans.push_back(root->right);
+            return NULL;
+        } 
+        return root;
+    }
+
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        vector<TreeNode*> ans;
+        unordered_set<int> st;
+        for(int i: to_delete) {
+            st.insert(i);
+        }
+        solve(root, st, ans);
+        if(st.find(root->val) == st.end()) {
+            ans.push_back(root);
+        }
+        return ans;
+    }
+};
