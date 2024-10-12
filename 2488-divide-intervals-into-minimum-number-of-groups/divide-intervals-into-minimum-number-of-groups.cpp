aea978dc6439f507a02835e0459301909a70a504
MayurJagtap096 <96325124+MayurJagtap096@users.noreply.github.com>
2024-10-12 16:53:59 +0530

    Time: 318 ms (49.55%) | Memory: 104.8 MB (61.71%) - LeetSync

---
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(vector<int> interval: intervals) {
+            int start = interval[0];
+            int end = interval[1];
+            if(!pq.empty() && pq.top() < start) {
+                pq.pop();
+            }
+            pq.push(end);
+        }
+        return pq.size();
+    }
+};
