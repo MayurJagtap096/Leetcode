a70e6f47f2f6ab30524f3295a7e1f22635d729b6
MayurJagtap096 <96325124+MayurJagtap096@users.noreply.github.com>
2025-01-25 12:55:32 +0530

    Time: 13 ms (8.90%) | Memory: 26.7 MB (33.35%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
+        // vectors to store the row sum and col sum
+        vector<int> rowServerSum(grid.size());
+        vector<int> colServerSum(grid[0].size());
         int ans=0;
-        int m=grid.size();
-        int n=grid[0].size();
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
+        for(int i=0;i<grid.size();i++) {
+            for(int j=0;j<grid[0].size();j++) {
+                rowServerSum[i] += grid[i][j];
+                colServerSum[j] += grid[i][j];
+            }
+        }
+        for(int i=0;i<grid.size();i++) {
+            for(int j=0;j<grid[0].size();j++) {
                 if(grid[i][j]==1) {
-                    bool canCommunicate = false;
-                    // we found a server
-                    // check if it can communicate with other server
-                    
-                    // firstly checking in the same row if the server is available
-                    for(int col=0;col<n;col++) {
-                        if(grid[i][col]==1 && col!=j) {
-                            canCommunicate = true;
-                            ans+=1;
-                            break;
-                        }
-                    }
-                    // If we dont find another server in same row then checking in the column
-                    if(!canCommunicate) {
-                        for(int row=0;row<m;row++) {
-                            if(grid[row][j]==1 && row!=i) {
-                                ans+=1;
-                                break;
-                            }
-                        }
-                    }
+                    if(rowServerSum[i]>1) ans++;
+                    else if(colServerSum[j]>1) ans++;
                 }
             }
         }
