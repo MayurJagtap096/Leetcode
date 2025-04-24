0893bec000baa705f296243b5cdfb3208cf528a1
MayurJagtap096 <96325124+MayurJagtap096@users.noreply.github.com>
2025-04-24 22:53:15 +0530

    Time: 366 ms (58.47%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
 
--- select score, dense_rank() over (order by score desc) as 'rank' from scores;
+select score, dense_rank() over (order by score desc) as 'rank' from scores;
 
-SELECT s1.score, 
-       (SELECT COUNT(DISTINCT s2.score) 
-        FROM Scores s2 
-        WHERE s2.score >= s1.score) AS 'rank'
-FROM Scores s1
-ORDER BY s1.score DESC;
+-- SELECT s1.score, 
+--        (SELECT COUNT(DISTINCT s2.score) 
+--         FROM Scores s2 
+--         WHERE s2.score >= s1.score) AS 'rank'
+-- FROM Scores s1
+-- ORDER BY s1.score DESC;
