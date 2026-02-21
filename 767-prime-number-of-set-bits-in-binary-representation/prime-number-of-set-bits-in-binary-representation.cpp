74220deb54812f2f84dfa5b07a2d20feb4f4a742
MayurJagtap096 <96325124+MayurJagtap096@users.noreply.github.com>
2026-02-21 23:33:40 +0530

    Time: 635 ms (5.00%) | Memory: 96 MB (5.09%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -11,7 +11,7 @@ public:
 
     bool isPrime(int n) {
         if(n==1) return false;
-        for(int i=2;i<n;i++) {
+        for(int i=2;i*i<=n;i++) {
             if(n%i==0) return false;
         }
         cout<<n<<endl;
