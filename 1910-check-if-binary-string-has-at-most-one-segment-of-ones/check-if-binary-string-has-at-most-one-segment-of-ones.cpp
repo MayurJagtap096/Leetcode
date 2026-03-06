adec9d6536b4aca2a4994d174720c8fe5c5e32dd
MayurJagtap096 <96325124+MayurJagtap096@users.noreply.github.com>
2026-03-06 14:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (10.47%) - LeetSync

---
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        for(int i=0;i<s.length()-1;i++) {
+            if(s[i]=='0' && s[i+1]=='1') return false; 
+        }
+        return true;
+    }
+};
